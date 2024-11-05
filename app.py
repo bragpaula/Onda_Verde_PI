bb3f5a3a8e90d7db3fba139155c185e7032396f0
jessicabr16 <133373725+jessicabr16@users.noreply.github.com>
2024-11-05 11:30:03 +0000

    reajustando

--- app.py
+++ app.py
@@ -1,48 +1,52 @@
-from flask import Flask, render_template,redirect, url_for, request,flash
+from flask import Flask, render_template, redirect, url_for, request, flash
 
 app = Flask(__name__)
-app.secret_key = 'prejetoI'
+app.secret_key = 'projetoI'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/acessarplataforma')
 def login():
     return render_template('acessarplataforma.html')
+
+@app.route('/decidir_cadastro', methods=['GET', 'POST'])
+def decidir_cadastro():
+    if request.method == 'POST':
+        tipo = request.form.get('tipo')
+        if tipo == 'cidadao':
+            return render_template('cadastro_cidadao.html')
+        elif tipo == 'ong':
+            return render_template('cadastro_ong.html')
+        elif tipo == 'empresa':
+            return render_template('cadastro_empresa.html')
+    return render_template('decidir_cadastro.html')
 
 @app.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html')
 
-@app.route('/decisao_cadastro')
-def decisao_cadastro():
-     if request.method == 'POST':
-        tipo = request.form.get('tipo')
-        if tipo == 'Cidadao':
-            return render_template('cadastro-cidadao.html')
-        elif tipo =='ong':
-            return render_template('cadastro-ong.html') 
-        elif tipo == 'Empresa':
-            return render_template('cadastro-empresa.html') 
-     return render_template('decisao-cadastro.html')
-
-
 @app.route('/verificarlogin', methods=['POST'])
-def verificarlogin():
-    return render_template('verificarlogin.html')
+def acessoverific():
+    usuario = request.form['usuario']
+    senha = request.form['senha']
+    if usuario == 'Jepale' and senha == 'Onda1234':
+        return redirect(url_for('perfil'))
+    else:
+        flash('Login ou senha inválidos')
+    return redirect(url_for('login'))
 
-@app.route('/perfil', methods=['GET','POST'])
+@app.route('/perfil', methods=['GET', 'POST'])
 def perfil():
     return render_template('perfil.html')
 
 @app.route('/sobrenos')
-def doacoes():
+def sobrenos():
     return render_template('sobrenos.html')
 
 @app.route('/adoteumamuda')
-def adote():
+def adotar():
     return render_template('adoteumamuda.html')
 
 @app.route('/feedbacks')
